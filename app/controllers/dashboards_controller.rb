91baf311ffe9e17fc43b935b4dd1fc0de28bcc67
Nicolas Martin <nicolas.artur.martin@gmail.com>
2019-11-21 10:35:21 +0100

    change in backend to include all and iterating on page dashboard

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,56 +1,87 @@
 class DashboardsController < ApplicationController
     def index
         @countries = select_countries
-        @customer_count = customer_count
-        @revenues = revenues
-        @average_revenue_per_order = average_revenue_per_order
-        @revenue_per_month = revenue_per_month
+        country = params[:country] ? params[:country] : "All" 
+        @customer_count = customer_count(country)
+        @revenues = revenues(country)
+        @average_revenue_per_order = average_revenue_per_order(country)
+        @revenue_per_month = revenue_per_month(country)
     end
 
     def select_countries
         array = []
         select_clause_countries = SalesDetail.select(:country).group(:country)
         array = select_clause_countries.map { |sales_detail| sales_detail.country }
+        array.sort
+        array.unshift("All")
         return array
     end
 
-    def customer_count
-        SalesDetail.from(SalesDetail.select(:customer_id, :country).distinct).group(:country).count
+    def customer_count(country)
+        customers = SalesDetail.from(SalesDetail.select(:customer_id, :country).distinct).group(:country).count
+        all_customers = SalesDetail.from(SalesDetail.select(:customer_id).distinct).count
+        if country == "All"
+            return all_customers
+        else 
+            return customers[country]
+        end
     end
 
-    def revenues
+    def revenues(country)
         request = "
             sales_details.country,
             SUM(sales_details.quantity * sales_details.unit_price) as total_revenue
         "
-        SalesDetail.select(request).group(:country).as_json(:except => :id)
-    end
-
-    def average_revenue_per_order
-        total_revenue = revenues
-        orders = order_count
-        array = []
-        revenues.each do |r|
-            average = {}
-            num_order = orders[r['country']]
-            average[:country] = r['country']
-            average[:average_rpo] = r['total_revenue'].fdiv(num_order)
-            array << average
+        if country == "All"
+            return SalesDetail.sum("quantity * unit_price")
+        else 
+            return SalesDetail.select(request).group(:country).as_json(:except => :id)
         end
-        return array
     end
 
-    def order_count
-        SalesDetail.from(SalesDetail.select(:order_id, :country).distinct).group(:country).count
+    def average_revenue_per_order(country)
+        total_revenue = revenues(country)
+        orders = order_count(country)
+        if country == "All"
+            return total_revenue.fdiv(orders)
+        else 
+            array = []
+            total_revenue.each do |r|
+                average = {}
+                num_order = orders[r['country']]
+                average[:country] = r['country']
+                average[:average_rpo] = r['total_revenue'].fdiv(num_order)
+                array << average
+            end
+            return array
+        end
+    end
+
+    def order_count(country)
+        orders = SalesDetail.from(SalesDetail.select(:order_id, :country).distinct).group(:country).count
+        all_orders = SalesDetail.from(SalesDetail.select(:order_id).distinct).count
+        if country == "All"
+            return all_orders
+        else 
+            return orders
+        end
     end
 
     
-    def revenue_per_month
+    def revenue_per_month(country)
         request = "
         sales_details.country, 
         CONCAT(date_part('year', sales_details.sale_date), '-', date_part('month', sales_details.sale_date)) as date_year_month, 
         SUM(sales_details.quantity * sales_details.unit_price) as monthly_revenue"
-        SalesDetail.select(request).group(:country, :'date_year_month').as_json(:except => :id)
+        global_request = "
+        CONCAT(date_part('year', sales_details.sale_date), '-', date_part('month', sales_details.sale_date)) as date_year_month, 
+        SUM(sales_details.quantity * sales_details.unit_price) as monthly_revenue
+        "
+        if country == "All"
+            return SalesDetail.select(global_request).group(:'date_year_month').as_json(:except => :id)
+        else 
+            return SalesDetail.select(request).group(:country, :'date_year_month').as_json(:except => :id)
+        end
     end
 end
 
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,22 @@
 import "bootstrap";
+import "jquery";
+
+
+const selectDiv = document.getElementById("select");
+
+if (selectDiv) {
+    console.log("toto");
+    $(document).ready(function() {
+        $('#select').change(function() {
+           $.ajax({
+             url: `/dashboards?country="${$("#select option:selected").val()}"`,
+             data: {"country": $("#select option:selected").val()},
+            //  dataType: "script",
+             method: "get",
+             success: function(r){}
+           });
+           console.log($("#select option:selected").val())
+         });
+       });
+};
+
