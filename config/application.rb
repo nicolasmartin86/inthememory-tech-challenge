d926321d67c1cd71a4d3f9d51cd16ee6d1bb43c1
Nicolas Martin <nicolas.artur.martin@gmail.com>
2019-11-21 19:12:19 +0100

    asset precompile value

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,7 @@ module InthememoryTechChallenge
 
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-    # config.assets.initialize_on_precompile = false
+    config.assets.initialize_on_precompile = false
     # config.serve_static_assets = true
 
     # Settings in config/environments/* take precedence over those specified here.
